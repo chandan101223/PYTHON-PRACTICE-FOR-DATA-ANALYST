05b3adb271c0af7d82cb13c7a33872264a3c697d
chandan101223 <167003721+chandan101223@users.noreply.github.com>
2024-04-15 23:05:35 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,24 @@
+# Integer: is a whole number positive or negative without decimas of unlimited length.
+print(12335385)
+
+# float:float or "floating point number" is a number positive or negative conataining one or more decimal.
+x=23.23
+print(x)
+
+#boolean:object of boolean tyoe may have one or two value.true or false:
+x=0
+y=bool(x)
+print(y)
+
+x=1
+y=bool(x)
+print(y)
+
+#String : strings in python are identified as a contiguous set of characters in the quation marks.Python allows for either pairs of single or double quotes.
+print('hello')
+
+# list: list is a collection that is ordered and changeable & allow duplicate members.
+list1=[1,2,3,'a','b',7,8]
+print(list1)
+
+
